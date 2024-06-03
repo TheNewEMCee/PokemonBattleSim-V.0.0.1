a2e0353b83033055d07a2d18f34d4787cc973a75
candiedBooks <christineeswann@gmail.com>
2024-06-03 10:10:30 -0400

    Can now remove specific lines
    WOOO I DID THE THING :DDDDD

--- src/Test.java
+++ src/Test.java
@@ -7,12 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Test {
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/christineswann/Documents/GitHub/JavaCppTest1/text/Data1.txt");
+        File file = new File("/Users/christineswann/Documents/GitHub/PokemonBattleSim-V.0.0.1/text/Data1.txt");
 
 
         //Reading:
@@ -33,19 +34,29 @@ public class Test {
             bw.newLine();
         } /* Adding new lines */
         PrintWriter pr = new PrintWriter(bw);
-        pr.println("data");
+        
         String line4 = Files.readAllLines(Paths.get(file.getAbsolutePath())).get(4);
         System.out.println(line4);
         int line4Length = (Files.readAllLines(Paths.get(file.getAbsolutePath())).get(4)).length();
+        
+
+        File inputFile = new File("/Users/christineswann/Documents/GitHub/PokemonBattleSim-V.0.0.1/text/Data1.txt");
+        File tempFile = new File("/Users/christineswann/Documents/GitHub/PokemonBattleSim-V.0.0.1/text/myTempFile.txt");
+        
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
         String currentLine = "hello";
         String lineToRemove;
-        while ((currentLine = br.readLine()) != null) {
+        while ((currentLine = reader.readLine()) != null) {
             String trimmedLine = currentLine.trim();
             lineToRemove = "hello";
             if (trimmedLine.equals(lineToRemove)) continue;
-            bw.write(currentLine + System.getProperty("line.separator"));
-
+            writer.write(currentLine + System.getProperty("line.separator"));
         }
+        writer.close(); 
+        reader.close(); 
+        boolean successful = tempFile.renameTo(inputFile);
 
         pr.write("newthings");
 
