9e4b7c2274912f13938ef665520c581423170230
TheNewEMCee <emilybmariamc@gmail.com>
2024-06-03 09:41:19 -0400

    Added Java Stuff

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    JInt a = new JInt();
+
+}
+
+class JInt{
+    String name;
+    int line;
+    int value;
+
+
+}
